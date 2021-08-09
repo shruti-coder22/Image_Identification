439297c00535f666dce687aa048e8234eeec2d3b
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-08-09 18:00:49 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -19,20 +19,3 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log("Image_Classifier loaded and connected!");
 }
-
-function check() {
-    img=document.getElementById("captured_image");
-    classifier.classify(img, gotResult);
-}
-
-function gotResult(error, results) {
-    if(error) {
-        console.error(error);
-    } 
-    else {
-        console.log(results)
-    }
-    document.getElementById("result_object_name").innerHTML=results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
-
-}
